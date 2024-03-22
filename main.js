9dbf0cb7dbad0742e9248c03b3a87b6b11177d92
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-22 08:53:10 -0700

    || Statment

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+//LOGICAL OPERATOR OR => ||
+//Question 1
+let city = "Karachi";
+let country = "Pakistan";
+console.log(city != country || city >= country);
+//                      true || false = true
+//Question 2
+let car = "BMW";
+let biCycle = "Unique";
+console.log(car == biCycle || car > biCycle);
+//                  false || false = false
+// Question 3
+let num1 = 34;
+let num2 = 78;
+console.log(num1 != num2 || num1 < num2);
+//                 true || true = true  
